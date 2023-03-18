826b74f289d00412121c0a80a47a95e46d354a31
Chazni Katz <Chazni.k9@gmail.com>
2023-03-19 00:52:09 +0200

    completed contains duplicate

---
+++ 01_ArraysAndHashing/ContainsDuplicate/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Example 1
+        //var nums = new int[] {1,2,3,1};
+
+        //Example 2
+        //var nums = new int[] {1,2,3,4};
+
+        //Example 3
+        var nums = new int[] {1,1,1,3,3,4,3,2,4,2};
+
+        var hasDuplicate = ContainsDuplicate(nums);
+        Console.WriteLine(hasDuplicate);
+    }
+   public static bool ContainsDuplicate(int[] nums) 
+   {
+        var numberDictionary = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (numberDictionary.ContainsKey(nums[i]))
+            {
+                return true;
+            }
+            else
+            {
+                numberDictionary.Add(nums[i], 1);
+            }
+        }
+        return false; 
+   }
+}
